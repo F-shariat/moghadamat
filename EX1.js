c4c381d4e4eaf086ffe6343480227e8846bf8b97
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-06-09 09:17:48 +0330

    Add files via upload

---
+++ EX1.js
@@ -0,0 +1,16 @@
+//video1-مقدماتی
+var course = "javascript"
+console.log(course)
+////////////////////////////////////
+var firstname;
+firstname = 'sara'
+console.log(firstname)
+////////////////////////////////
+var X = 'JS' ,
+    Y = 'tara' , 
+    Z = 25
+console.log('X')
+console.log('Y')
+console.log('Z')
+
+
