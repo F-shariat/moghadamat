566df95f43601cce2feb58064df1c74b410ab03e
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-06-12 00:01:04 +0330

    Add files via upload

---
+++ EX4.js
@@ -0,0 +1,39 @@
+//video4-مقدماتی
+
+//Data types 
+//String
+let Name = "sara";
+console.log(Name)
+console.log(typeof Name);
+
+//Number
+let Z = 20
+console.log(Z)
+console.log(typeof Z);
+
+let Y = "54"
+console.log(Y)
+console.log(typeof Y);
+
+//تبدیل رشته عددی به عدد
+let variable = "65"
+let X = Number(variable)
+console.log(variable)
+console.log(typeof variable)
+
+//object
+let personal={
+    firstName : 'faeze' ,
+    lastName : 'shariat' ,
+    age : 21
+}
+console.log(personal)
+console.log(typeof personal);
+//دسترسی به عضو خاصی از object
+//console.log(personal.firstName)
+//console.log(personal['firstName)'])
+
+//symbol
+let mark = Symbol('world')
+console.log(mark)
+console.log(typeof mark);
