46b258d83084cb1ddc8c2e84cede3f3f79cd06b4
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-06-21 01:44:08 +0330

    Add files via upload

---
+++ EX5.js
@@ -0,0 +1,42 @@
+//video5-مقدماتی
+//Array
+let A = ['Faeze' , 21 , 'JS Developer']
+console.log(A)
+console.log(typeof A)
+console.log(Array.isArray(A))
+
+//function
+let tabe = function(){
+    console.log("new function")
+}
+tabe() //  فراخوانی تابع
+console.log(tabe)
+console.log(typeof tabe)
+
+//Boolean
+let variable = false
+console.log(variable)
+console.log(typeof variable)
+
+K=Boolean(12>8)
+console.log(K)
+
+Z = (4>16)
+console.log(Z)
+console.log(typeof Z)
+
+//date
+
+let date = new Date()
+console.log(date)
+console.log(typeof date)
+
+//undefind
+let R;
+console.log(R)
+console.log(typeof R)
+
+//null
+let vacant = null
+console.log(vacant)
+console.log(typeof vacant)
