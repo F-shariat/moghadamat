d77448b5415676daed4cb0f5c0cd1c2404e81dd2
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-06-26 00:28:57 +0330

    Add files via upload

---
+++ EX9,10.js
@@ -0,0 +1,35 @@
+//concatenation string
+let s1 = 'Welcome'
+let s2 = 'to our website'
+let R = s1 + " " + s2
+console.log(R)
+
+/* let s1 = 'Welcome'
+let s2 = 'to our website'
+console.log(s1+ " " + s2)*/
+
+ s1 = 'Welcome'
+ s2 = 'to our website'
+ R =  "Sara" + " " + s1 + " " + s2
+console.log(R)
+
+//Length
+let s3 = "hello world!"
+let result;
+result = s3.length
+console.log(result)
+
+//Concat
+let s4 = "Say hello to the"
+let output = s4.concat(',' , "world")
+console.log(output)
+
+//uppercasse
+let s5 = "javascript"
+let X = s5.toUpperCase()
+console.log(s5)
+
+//Lowercase
+let s6 = "NODE.JS"
+let Y = s6.toLowerCase()
+console.log(Y)
