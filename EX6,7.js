15f3a1d7b03c27c90d5dd2e5cabbc1b261430e82
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-06-26 00:13:19 +0330

    Add files via upload

---
+++ EX6,7.js
@@ -0,0 +1,49 @@
+//video 6 , 7-مقدماتی
+//Arithmetic
+
+let num1 = 55
+let num2 = 10
+let num3 = '15'
+let N;
+
+N = num1+num2
+N = num1-num2
+N = num1*num2
+N = num1/num2
+N = num1%num2
+
+console.log(N) //اخرین مقدار که داخلش ریخته شده را نمایش میدهد
+console.log(num1)
+console.log(num2)
+console.log(num3)
+console.log(typeof num3)
+
+//Assignment
+num1 = num1-num2
+//num1 -= num2
+console.log(num1)
+
+//comprison
+N = num1 == num2; // مقادیر رو با هم مقایسه میکنه و true یا falseبرمیگردونه
+console.log(N)
+
+N = num2 === num3; //نوع رو با هم مقایسه میکنه و true یا false برمیگردونه
+console.log(N)
+
+//not
+N = num1 != num2 //مقایسه مقدار
+console.log(N)
+
+N = num1 !== num3 //مقایسه نوع
+console.log(N)
+
+//Logical
+N = (num2<num1 && num2>50)
+console.log(N)
+
+N = (num2>num1 || num1>50)
+console.log(N)
+
+
+
+
