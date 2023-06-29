f07588f1d473338d34ab73b9820621cbadfb759c
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-06-30 00:51:14 +0330

    Add files via upload

---
+++ EX11,12.js
@@ -0,0 +1,39 @@
+//video11 - مقدماتی
+//...Strings-methods
+
+//indexof
+let input1 =  "javascript programmer"
+let output1 = input1.indexOf('c')
+console.log(output1)
+
+//includes
+let input2 =  "javascript programmer"
+let output2 = input2.includes('Java')
+console.log(output2)
+
+//substring
+let input3 =  "javascript programmer"
+let output3 = input3.substring(0,4)
+console.log(output3)
+
+//video12
+//slice
+let input4 =  "javascript programmer"
+let output4 = input4.slice(4)
+console.log(output4)
+
+//split
+let input5 =  "javascript,programmer"
+let output5 = input5.split(',')
+console.log(output5)
+
+//replase
+let input6 =  'hello world'
+let output6 = input6.replace('hello' , 'Hi')
+console.log(output6)
+
+//repeat
+let input7 =  "welcome to our website"
+let output7 = input7.repeat(3)
+console.log(output7)
+
