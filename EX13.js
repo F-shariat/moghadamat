306429dcc284a3e666cba711ded4c48083c5ce33
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-07-01 11:10:26 +0330

    Add files via upload

---
+++ EX13.js
@@ -0,0 +1,40 @@
+//video13-مقدماتی
+//numbers-methods
+
+//pi
+let num1;
+num1 = Math.PI
+console.log(num1)
+
+//round
+let num2 = 13.2;
+let R2 = Math.round(num2)
+console.log(R2)
+
+//ceil
+let num3 = 14.1;
+let R3 = Math.ceil(num3)
+console.log(R3)
+
+//floor
+let num4 = 17.7
+let R4 = Math.floor(num4)
+console.log(R4)
+
+//sqrt
+let R5 = Math.sqrt(25)
+console.log(R5)
+
+//abs
+let R6 = Math.abs(-10)
+console.log(R6)
+
+//min
+let num7 = (1,2,-5,8,-9);
+let R7;
+R7 = Math.min(num7);
+console.log(R7)
+
+//max
+let R8 = Math.max(10,-3,5,32);
+console.log(R8)
