82f0fa53ed834a951a0bc08cc8745478b23a0b85
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-07-02 22:46:27 +0330

    Add files via upload

---
+++ EX14.js
@@ -0,0 +1,42 @@
+//video14-مقدماتی
+//...numbers-methods
+
+//to string
+let num9 = 20
+let R9 = num9.toString()
+console.log(R9)
+console.log(typeof R9)
+
+//tofixed
+let num10 = 17.758412
+let R10 ;
+R10 = num10.toFixed(3)
+console.log(R10)
+
+//to precision
+let num11 = 14528.562
+let R11 = num11.toPrecision(3)
+console.log(R11)
+
+//Number
+let num12 = '10'
+let R12 = Number(num12)
+console.log(R12)
+
+//parselnt
+/*parselnt("2023") //return 2023
+parselnt("20.23")    //20
+parselnt("20 23 24") //20
+parselnt("20 years") //20
+parselnt("years 20")*/  //NaN
+
+//parsefloat
+/*parsefloat("2023") //2023
+parsefloat("20.23") //20.23
+parsefloat("20 23 25") //20
+parsefloat("20 years ")  //20   
+parsefloat("years 20") */  //NaN
+
+
+
+
