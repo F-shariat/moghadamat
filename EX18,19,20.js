5165a802b60e487f35dda8be320bb1b58b81b616
F-shariat <121151564+F-shariat@users.noreply.github.com>
2023-07-04 01:16:39 +0330

    Add files via upload

---
+++ EX18,19,20.js
@@ -0,0 +1,24 @@
+//video18-مقدماتی
+//..Array
+
+
+//sort
+let fruit1 = ['apple' , 'banana' , 'Portugal' , 'kiwi', 'Mulberry']
+fruit1.sort()
+console.log(fruit1)
+
+//reverse
+let fruit2 = ['apple' , 'banana' , 'Portugal' , 'kiwi', 'Mulberry']
+fruit2.sort()
+console.log(fruit2)
+
+//video19
+//object
+let p ={
+    firstname : 'maryam' ,
+    lastname : 'ahmadi' ,
+    ege : 25 ,
+    color : 'purple'
+}
+p.ege = 28
+console.log(p)
